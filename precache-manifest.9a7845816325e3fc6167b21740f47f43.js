6e50572fac5ee9d757966e0bf76788c298a12591
Aleksandr <sanyamaks14@gmail.com>
2020-02-28 22:09:51 +0400

    Updates

---
+++ precache-manifest.9a7845816325e3fc6167b21740f47f43.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dffeed7796e7c46d2a4a13828ed4577",
+    "url": "/test_task_anmedio/index.html"
+  },
+  {
+    "revision": "2742ff7e65ad8cd980a0",
+    "url": "/test_task_anmedio/static/css/main.18e2b03d.chunk.css"
+  },
+  {
+    "revision": "e328b6dfda2c05cfcd95",
+    "url": "/test_task_anmedio/static/js/2.be71ce74.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/test_task_anmedio/static/js/2.be71ce74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2742ff7e65ad8cd980a0",
+    "url": "/test_task_anmedio/static/js/main.ebe6b829.chunk.js"
+  },
+  {
+    "revision": "8cedc7a8bdfc46e45df0",
+    "url": "/test_task_anmedio/static/js/runtime-main.29c29746.js"
+  },
+  {
+    "revision": "5390828f32f017cbe4ee17931d7cbc6d",
+    "url": "/test_task_anmedio/static/media/CeraPro-Bold.5390828f.ttf"
+  },
+  {
+    "revision": "808440a0a16864b0e8a6f9659217c7e6",
+    "url": "/test_task_anmedio/static/media/CeraPro-Bold.808440a0.woff"
+  },
+  {
+    "revision": "f6ee8066225b9e1141916063643b855e",
+    "url": "/test_task_anmedio/static/media/CeraPro-Bold.f6ee8066.eot"
+  },
+  {
+    "revision": "12cb515cf96257eb006975f7ec5c251a",
+    "url": "/test_task_anmedio/static/media/CeraPro-Medium.12cb515c.eot"
+  },
+  {
+    "revision": "bafc951d7d329580e96345cc473c4d3b",
+    "url": "/test_task_anmedio/static/media/CeraPro-Medium.bafc951d.ttf"
+  },
+  {
+    "revision": "f095019108fd2d78d76b6575a2af80de",
+    "url": "/test_task_anmedio/static/media/CeraPro-Medium.f0950191.woff"
+  },
+  {
+    "revision": "8fa6dc48a21663687f973f5cb8b1c111",
+    "url": "/test_task_anmedio/static/media/CeraPro-Regular.8fa6dc48.woff"
+  },
+  {
+    "revision": "d845af5681386c04bec48b4b918c2df8",
+    "url": "/test_task_anmedio/static/media/CeraPro-Regular.d845af56.eot"
+  },
+  {
+    "revision": "f582d3a21e59803eb6af6411dd730f32",
+    "url": "/test_task_anmedio/static/media/CeraPro-Regular.f582d3a2.ttf"
+  },
+  {
+    "revision": "4d6710f989442628a9acdf9a5e5b27c5",
+    "url": "/test_task_anmedio/static/media/comeback.4d6710f9.svg"
+  },
+  {
+    "revision": "b7110cd9ff88c609758f938fb075190c",
+    "url": "/test_task_anmedio/static/media/decrement.b7110cd9.svg"
+  },
+  {
+    "revision": "e9b601d90210641bf5feb6bfef4b9b3e",
+    "url": "/test_task_anmedio/static/media/flag.e9b601d9.svg"
+  },
+  {
+    "revision": "76e230847f2df308cc004cb545f5ed2a",
+    "url": "/test_task_anmedio/static/media/increment.76e23084.svg"
+  },
+  {
+    "revision": "e6f2d1527bfb429d13ff91ec5aae7985",
+    "url": "/test_task_anmedio/static/media/main-illustration.e6f2d152.svg"
+  },
+  {
+    "revision": "df05ae8807bfb8e3ddb210db1ff7cc8e",
+    "url": "/test_task_anmedio/static/media/many-bottles.df05ae88.svg"
+  },
+  {
+    "revision": "7be0fc944d9a8f813ffcf3cee4c78146",
+    "url": "/test_task_anmedio/static/media/one-bottle.7be0fc94.svg"
+  },
+  {
+    "revision": "f28366e77fcb88d2a0ca525ca907ea3c",
+    "url": "/test_task_anmedio/static/media/report__icon.f28366e7.svg"
+  },
+  {
+    "revision": "f912fbdfb55a1735774cc5b458f35043",
+    "url": "/test_task_anmedio/static/media/shape.f912fbdf.svg"
+  }
+]);
